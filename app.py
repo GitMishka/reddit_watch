d6cc2b107d5c5b094b8f7557c53ada943c1e68e8
Misha <mishagrigoryan@gmail.com>
2023-05-20 23:32:04 -0400

    work in progres

--- app.py
+++ app.py
@@ -2,73 +2,106 @@ import praw
 from tabulate import tabulate
 from datetime import datetime
 from twilio.rest import Client
+import psycopg2
+import time
 
+while True:
+  try:
+# Connect to Postgres database
+    conn = psycopg2.connect(
+        host="database-1.cueq5a3aruqx.us-east-2.rds.amazonaws.com",
+        database="db1",
+        user="postgres",
+        password=""
+    )
+    cur = conn.cursor()
 
-user_agent = "Searchbot_01"
-reddit = praw.Reddit(username="Searchbot_01",
-                     password ="aaaa1111",
-                     client_id="Ai32qfXNqvGuMEvHFFMlAw",
-                     client_secret="IG5XKjyUGkcG2cgXfBSwVvalMTxFRg",
-                     user_agent=user_agent,
-                     check_for_async=False)
+    # Create table if it doesn't exist
+    cur.execute("CREATE TABLE IF NOT EXISTS watch_exchange_posts (id TEXT PRIMARY KEY, title TEXT, post_time TIMESTAMP, url TEXT, price FLOAT, brand TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS texted_watch_exchange_posts (id TEXT PRIMARY KEY, title TEXT, post_time TIMESTAMP, url TEXT, price FLOAT, brand TEXT)")
 
-subreddit = reddit.subreddit('watchexchange')
-posts = subreddit.new(limit=10)
+    # Set up Twilio
+    auth_token = ''
+    client = Client(account_sid, auth_token)
 
-search_terms = ['omega', 'sinn','rolex']  # List of search terms
-brands  = ['A. Lange & Sohne', 'Alpina', 'Armani Exchange', 'Audemars Piguet', 
-                'Ball', 'Baume et Mercier', 'Bell & Ross', 'Blancpain', 'Breguet', 'Breitling', 
-                'Bremont', 'Bulova', 'Bvlgari', 'Cartier', 'Certina', 'Chanel', 'Chopard', 'Citizen', 
-                'Corum', 'Daniel Wellington', 'De Bethune', 'Doxa', 'Ebel', 'Eberhard & Co.', 
-                'F.P. Journe', 'Fossil', 'Frederique Constant', 'Girard-Perregaux', 'Glashütte Original', 
-                'G-Shock', 'Hamilton', 'Harry Winston', 'Hermes', 'Hublot', 'IWC', 'Jaeger-LeCoultre', 'Junghans', 
-                'Laco', 'Longines', 'Maurice Lacroix', 'MeisterSinger', 'Montblanc', 'Movado', 'Nomos Glashütte', 'Omega', 
-                'Oris', 'Panerai', 'Parmigiani Fleurier', 'Patek Philippe', 'Piaget', 'Rado', 'Raymond Weil', 'Richard Mille', 
-                'Rolex', 'Seiko', 'Shinola', 'Sinn', 'Tag Heuer', 'Tissot', 'Tudor', 'Ulysse Nardin', 'Vacheron Constantin', 
-                'Victorinox Swiss Army', 'Zenith']
+    # Set up Reddit
+    user_agent = "Searchbot_01"
+    reddit = praw.Reddit(username="Searchbot_01",
+                        password ="aaaa1111",
+                        client_id="Ai32qfXNqvGuMEvHFFMlAw",
+                        client_secret="IG5XKjyUGkcG2cgXfBSwVvalMTxFRg",
+                        user_agent=user_agent,
+                        check_for_async=False)
+    subreddit = reddit.subreddit('watchexchange')
+    posts = subreddit.new(limit=10)
 
-table = []
-for post in posts:
-    post_name = post.title.lower()  # Convert post name to lowercase
-    brand = ''
-    for b in brands:
-        if b.lower() in post_name:
-            brand = b
-            break
-    # Check if any of the search terms are in the lowercase post name
-    if any(term in post_name for term in search_terms):
-        # Extract price from post name (assuming price follows after "$" and may include "K", "k", or ".")
+    # Define search terms and brand list
+    # Define search terms and brand list
+    global search_terms
+    search_terms = ['omega', 'sinn', 'rolex', 'seiko']
+
+    brand_list = ['A. Lange & Sohne', 'Alpina', 'Armani Exchange', 'Audemars Piguet', 
+                    'Ball', 'Baume et Mercier', 'Bell & Ross', 'Blancpain', 'Breguet', 'Breitling', 
+                    'Bremont', 'Bulova', 'Bvlgari', 'Cartier', 'Certina', 'Chanel', 'Chopard', 'Citizen', 
+                    'Corum', 'Daniel Wellington', 'De Bethune', 'Doxa', 'Ebel', 'Eberhard & Co.', 
+                    'F.P. Journe', 'Fossil', 'Frederique Constant', 'Girard-Perregaux', 'Glashütte Original', 
+                    'G-Shock', 'Hamilton', 'Harry Winston', 'Hermes', 'Hublot', 'IWC', 'Jaeger-LeCoultre', 'Junghans', 
+                    'Laco', 'Longines', 'Maurice Lacroix', 'MeisterSinger', 'Montblanc', 'Movado', 'Nomos Glashütte', 'Omega', 
+                    'Oris', 'Panerai', 'Parmigiani Fleurier', 'Patek Philippe', 'Piaget', 'Rado', 'Raymond Weil', 'Richard Mille', 
+                    'Rolex', 'Seiko', 'Shinola', 'Sinn', 'Tag Heuer', 'Tissot', 'Tudor', 'Ulysse Nardin', 'Vacheron Constantin', 
+                    'Victorinox Swiss Army', 'Zenith']
+
+    # Iterate through posts
+    for post in posts:
+        post_id = post.id
+        post_name = post.title.lower()
+        post_time = datetime.utcfromtimestamp(post.created_utc)
+        url = post.url
         price_str = post_name.split("$")[-1].split()[0]
         try:
             price = float(price_str.replace("k", "000").replace("K", "000").replace(".", ""))
         except ValueError:
-            price = ''
-        post_time = datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S')
-        table.append([post_name, post_time, post.url, brand, price])
+            price = None
+        brand = None
+        for brand_name in brand_list:
+            if brand_name.lower() in post_name:
+                brand = brand_name
+                break
+        
+        # Check if post ID already exists in database
+        cur.execute("SELECT id FROM watch_exchange_posts WHERE id=%s", (post_id,))
+        existing_post = cur.fetchone()
+        if existing_post:
+            continue
+        
+        # Insert new row into database
+        cur.execute("INSERT INTO watch_exchange_posts (id, title, post_time, url, price, brand) VALUES (%s, %s, %s, %s, %s, %s)",
+                    (post_id, post_name, post_time, url, price, brand))
+        conn.commit()
+        
+        if any(term in post_name for term in search_terms):
+            # Check if post ID already exists in texted_watch_exchange_posts
+            cur.execute("SELECT id FROM texted_watch_exchange_posts WHERE id=%s", (post_id,))
+            texted_post = cur.fetchone()
+            
+            if not texted_post:
+                message_body = f"Brand: {brand}\nTime: {post_time}\nLink: {url}"
+                message = client.messages.create(
+                    to='14232814382',
+                    from_='18334633894',
+                    body=message_body
+                )
+                print(f"Message sent with ID: {message.sid}")
+                
+                # Insert new row into texted_watch_exchange_posts
+                cur.execute("INSERT INTO texted_watch_exchange_posts (id, title, post_time, url, price, brand) VALUES (%s, %s, %s, %s, %s, %s)",
+                            (post_id, post_name, post_time, url, price, brand))
+                conn.commit()
+            
+  except Exception as e:
+        # handle any exceptions that may be raised
+      print(f"An error occurred: {e}")
+  #print(message_body) 
+  time.sleep(10) # sleep for 10 seconds before running the loop again
 
-headers = ['Post Name', 'Post Time', 'Post Link', 'Brand', 'Price']
-
-table_str = tabulate(table, headers=headers)  # Convert table to string
-
-# Twilio account information
-account_sid = 'ACd2079bd46f13974225b28ef06255af01'
-auth_token = '8dc2962417449a63b380cd0fbaec99f0'
-client = Client(account_sid, auth_token)
-
-# Send message with table of results using Twilio
-
-phone_numbers = ['14239338894'] #['14232272113','14232814382']
-from_numbers = ['18334633894']
-for row in table:
-    if any(b in row for b in brands):
-        #message_body = f"Brand: {row[3]}"
-        message_body = f"Brand: {row[3]}\nLink: {row[2]}\nPost Time: {row[1]}"
-
-        message = client.messages.create(
-            to='14232814382',
-            from_='18334633894',
-            body= message_body
-        )
-        print(f"Message sent with ID: {message.sid}")
-
-print(f"Table:\n{table_str}")
+            
